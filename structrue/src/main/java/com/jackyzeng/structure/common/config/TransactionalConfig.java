21f7326c21a246080f13b164580dcc0fa5b48ada
Jacky Zeng <jinzengjacky@qq.com>
2022-03-12 23:19:23 +0800

    Add transactional config

---
+++ structrue/src/main/java/com/jackyzeng/structure/common/config/TransactionalConfig.java
@@ -0,0 +1,9 @@
+package com.jackyzeng.structure.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionalConfig {
+}
