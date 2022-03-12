51d3e269eff6400cedda8717751a29d50c79f2b5
Jacky Zeng <jinzengjacky@qq.com>
2022-03-12 15:21:48 +0800

    Update Project

---
+++ structrue/src/main/java/com/jackyzeng/structure/common/aspect/LogAspect.java
@@ -0,0 +1,44 @@
+package com.jackyzeng.structure.common.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+
+    @Pointcut("execution(* com.jackyzeng.structure.db.controller..*.*(..))")
+    public void pointCut(){
+
+    }
+
+    @Before("pointCut()")
+    public void before() {
+        log.info("Before log");
+    }
+
+    @After("pointCut()")
+    public void after() {
+        log.info("After log");
+    }
+
+    @AfterReturning("pointCut()")
+    public void afterReturning() {
+        log.info("After returning log");
+    }
+
+    @AfterThrowing("pointCut()")
+    public void afterThrowing() {
+        log.info("After throwing log");
+    }
+
+    @Around("pointCut()")
+    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Before Around");
+        joinPoint.proceed();
+        log.info("After around");
+    }
+}
--- structrue/src/main/java/com/jackyzeng/structure/redis/RedisConfig.java
+++ structrue/src/main/java/com/jackyzeng/structure/redis/RedisConfig.java
@@ -1,8 +1,13 @@
 package com.jackyzeng.structure.redis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
@@ -10,7 +15,10 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Bean
@@ -29,6 +37,16 @@ public class RedisConfig {
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+        redisCacheConfiguration = redisCacheConfiguration.entryTtl(Duration.ofHours(1))
+                .disableCachingNullValues();
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(factory))
+                .cacheDefaults(redisCacheConfiguration).build();
+    }
+
 
     @Bean
     public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory factory) {
