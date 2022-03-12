11f247a485c7261eb7e0adaa6a2d4a1890dddf9e
Jacky Zeng <jinzengjacky@qq.com>
2022-03-12 21:03:57 +0800

    update project

--- structrue/src/main/java/com/jackyzeng/structure/StructureApplication.java
+++ structrue/src/main/java/com/jackyzeng/structure/StructureApplication.java
@@ -2,14 +2,11 @@ package com.jackyzeng.structure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 public class StructureApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StructureApplication.class, args);
     }
-
 }
