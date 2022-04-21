13f2c06eb50cf080efb6a5b7e13cf9cf5e572327
Jacky Zeng <Jin.JJ.Zeng@ibm.com>
2022-04-21 11:09:00 +0800

    add enum template

--- structrue/src/main/java/com/jackyzeng/structure/common/aspect/LogAspect.java
+++ structrue/src/main/java/com/jackyzeng/structure/common/aspect/LogAspect.java
@@ -5,40 +5,40 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
-@Slf4j
-@Aspect
-@Component
+//@Slf4j
+//@Aspect
+//@Component
 public class LogAspect {
 
-    @Pointcut("execution(* com.jackyzeng.structure.db.controller..*.*(..))")
-    public void pointCut(){
-
-    }
-
-    @Before("pointCut()")
-    public void before() {
-        log.info("Before log");
-    }
-
-    @After("pointCut()")
-    public void after() {
-        log.info("After log");
-    }
-
-    @AfterReturning("pointCut()")
-    public void afterReturning() {
-        log.info("After returning log");
-    }
-
-    @AfterThrowing("pointCut()")
-    public void afterThrowing() {
-        log.info("After throwing log");
-    }
-
-    @Around("pointCut()")
-    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("Before Around");
-        joinPoint.proceed();
-        log.info("After around");
-    }
+//    @Pointcut("execution(* com.jackyzeng.structure.db.controller..*.*(..))")
+//    public void pointCut(){
+//
+//    }
+//
+//    @Before("pointCut()")
+//    public void before() {
+//        log.info("Before log");
+//    }
+//
+//    @After("pointCut()")
+//    public void after() {
+//        log.info("After log");
+//    }
+//
+//    @AfterReturning("pointCut()")
+//    public void afterReturning() {
+//        log.info("After returning log");
+//    }
+//
+//    @AfterThrowing("pointCut()")
+//    public void afterThrowing() {
+//        log.info("After throwing log");
+//    }
+//
+//    @Around("pointCut()")
+//    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+//        log.info("Before Around");
+//        joinPoint.proceed();
+//        log.info("After around");
+//    }
 }
---
+++ structrue/src/main/java/com/jackyzeng/structure/enums/EnumTemplate.java
@@ -0,0 +1,45 @@
+package com.jackyzeng.structure.enums;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum EnumTemplate {
+    ENUM1("Hi") {
+        @Override
+        public void method() {
+
+        }
+    },
+    ENUM2("Hello"){
+        @Override
+        public void method() {
+
+        }
+    };
+
+    private final String value;
+
+    EnumTemplate(String value) {
+        this.value = value;
+    }
+
+    public abstract void method();
+
+    // If Override toString()
+    // Implementing a fromString method on an enum type
+    private static final Map<String, EnumTemplate> stringToEnum =
+            Stream.of(values()).collect(
+                    Collectors.toMap(Object::toString, e -> e));
+
+    // Returns Operation for string, if any
+    public static Optional<EnumTemplate> fromString(String symbol) {
+        return Optional.ofNullable(stringToEnum.get(symbol));
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
