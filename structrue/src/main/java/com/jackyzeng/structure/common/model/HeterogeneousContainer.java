f44df9d50353361203a830c023cd891a612dacc9
Jacky Zeng <Jin.JJ.Zeng@ibm.com>
2022-05-05 13:54:51 +0800

    add heterogeneous container demo

---
+++ structrue/src/main/java/com/jackyzeng/structure/common/model/HeterogeneousContainer.java
@@ -0,0 +1,17 @@
+package com.jackyzeng.structure.common.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HeterogeneousContainer {
+    private Map<Class<?>, Object> container = new HashMap<>();
+
+    public <T> void put(Class<T> type, T instance ) {
+        container.put(Objects.requireNonNull(type), instance);
+    }
+
+    public <T> T get(Class<T> type) {
+        return type.cast(container.get(type));
+    }
+}
