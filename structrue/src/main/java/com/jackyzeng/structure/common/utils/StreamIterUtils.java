da96d2b369363a2f157aa387308ed4dfa779cb00
Jacky Zeng <Jin.JJ.Zeng@ibm.com>
2022-05-06 16:42:46 +0800

    add stream iterable utils

---
+++ structrue/src/main/java/com/jackyzeng/structure/common/utils/StreamIterUtils.java
@@ -0,0 +1,15 @@
+package com.jackyzeng.structure.common.utils;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class StreamIterUtils {
+
+    public static <E> Iterable<E> iterableOf(Stream<E> stream) {
+        return stream::iterator;
+    }
+
+    public static <E> Stream<E> streamOf(Iterable<E> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+}
