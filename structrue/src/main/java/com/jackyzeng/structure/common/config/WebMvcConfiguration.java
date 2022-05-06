17e6c95b4226c75914cae87837d8a2dd4d016925
Jacky Zeng <Jin.JJ.Zeng@ibm.com>
2022-05-06 10:09:43 +0800

    add converter demo

---
+++ structrue/src/main/java/com/jackyzeng/structure/common/config/WebMvcConfiguration.java
@@ -0,0 +1,27 @@
+package com.jackyzeng.structure.common.config;
+
+import com.jackyzeng.structure.common.model.ConverterTarget;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration {
+    
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addFormatters(FormatterRegistry registry) {
+                registry.addConverter(new Converter<String, ConverterTarget>() {
+                    @Override
+                    public ConverterTarget convert(String s) {
+                        return new ConverterTarget();
+                    }
+                });
+            }
+        };
+    }
+}
---
+++ structrue/src/main/java/com/jackyzeng/structure/common/model/ConverterTarget.java
@@ -0,0 +1,7 @@
+package com.jackyzeng.structure.common.model;
+
+import lombok.Data;
+
+@Data
+public class ConverterTarget {
+}
