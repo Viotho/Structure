53d0321dce04f0b05a7052d5ef2d2dcb9a902919
Jacky Zeng <Jin.JJ.Zeng@ibm.com>
2022-05-09 10:24:40 +0800

    silent stream

--- structrue/src/main/java/com/jackyzeng/structure/common/message/CustomSource.java
+++ structrue/src/main/java/com/jackyzeng/structure/common/message/CustomSource.java
@@ -1,9 +1,6 @@
 package com.jackyzeng.structure.common.message;
 
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.messaging.MessageChannel;
-
 public interface CustomSource {
-    @Output("customOutput")
-    MessageChannel customOutput();
+//    @Output("customOutput")
+//    MessageChannel customOutput();
 }
--- structrue/src/main/java/com/jackyzeng/structure/common/message/ReceiveService.java
+++ structrue/src/main/java/com/jackyzeng/structure/common/message/ReceiveService.java
@@ -1,13 +1,10 @@
 package com.jackyzeng.structure.common.message;
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-
-@EnableBinding(CustomSink.class)
+//@EnableBinding(CustomSink.class)
 public class ReceiveService {
 
-    @StreamListener("customInput")
-    public void receiveMessage(Object payload) {
-        System.out.println(payload);
-    }
+//    @StreamListener("customInput")
+//    public void receiveMessage(Object payload) {
+//        System.out.println(payload);
+//    }
 }
