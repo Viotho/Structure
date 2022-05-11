63d9fe395d88292e4bcece3280ba9c42ef538f61
Jacky Zeng <Jin.JJ.Zeng@ibm.com>
2022-05-11 16:48:59 +0800

    update

--- structrue/src/main/java/com/jackyzeng/structure/common/config/WebMvcConfiguration.java
+++ structrue/src/main/java/com/jackyzeng/structure/common/config/WebMvcConfiguration.java
@@ -1,15 +1,11 @@
 package com.jackyzeng.structure.common.config;
 
-import com.jackyzeng.structure.common.converter.CustomMessageConverter;
 import com.jackyzeng.structure.common.model.ConverterTarget;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 public class WebMvcConfiguration {
@@ -17,6 +13,7 @@ public class WebMvcConfiguration {
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addFormatters(FormatterRegistry registry) {
                 registry.addConverter(new Converter<String, ConverterTarget>() {
@@ -27,10 +24,10 @@ public class WebMvcConfiguration {
                 });
             }
 
-            @Override
-            public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-                converters.add(new CustomMessageConverter());
-            }
+//            @Override
+//            public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//                converters.add(new CustomMessageConverter());
+//            }
         };
     }
 }
