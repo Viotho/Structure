0bc1c434b7efc2a229af21ba54b28097e210c7f9
Jacky Zeng <jinzengjacky@qq.com>
2022-06-22 23:04:45 +0800

    add interceptor demo

--- structure/src/main/java/com/jackyzeng/structure/common/config/WebMvcConfiguration.java
+++ structure/src/main/java/com/jackyzeng/structure/common/config/WebMvcConfiguration.java
@@ -1,33 +1,39 @@
 package com.jackyzeng.structure.common.config;
 
+import com.jackyzeng.structure.common.converter.CustomMessageConverter;
+import com.jackyzeng.structure.common.interceptor.CustomInterceptor;
 import com.jackyzeng.structure.common.model.ConverterTarget;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
-public class WebMvcConfiguration {
-    
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
+public class WebMvcConfiguration implements  WebMvcConfigurer {
 
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new Converter<String, ConverterTarget>() {
             @Override
-            public void addFormatters(FormatterRegistry registry) {
-                registry.addConverter(new Converter<String, ConverterTarget>() {
-                    @Override
-                    public ConverterTarget convert(String s) {
-                        return new ConverterTarget();
-                    }
-                });
+            public ConverterTarget convert(String s) {
+                return new ConverterTarget();
             }
+        });
+    }
 
-//            @Override
-//            public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-//                converters.add(new CustomMessageConverter());
-//            }
-        };
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new CustomMessageConverter());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CustomInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login", "/css/**", "/fonts/**", "/js/**", "/images/**");
     }
 }
